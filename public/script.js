f5b80e157d135a8761e9bb3a7aaa822f9493149d
taitas2 <taitigui@gmail.com>
2026-03-05 14:48:10 -0300

    site surpresa livia

---
+++ public/script.js
@@ -0,0 +1,16 @@
+function mostrarMensagem(){
+
+    const texto = `
+    Livia,
+    
+    Esse site foi feito só para você.
+    
+    Obrigado por cada momento
+    e por fazer minha vida melhor.
+    
+    Te amo muito ❤️
+    `
+    
+    document.getElementById("mensagem").innerText = texto
+    
+    }
