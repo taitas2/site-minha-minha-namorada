fdd364aea88256e430390b6a3685787816477b74
taitas2 <taitigui@gmail.com>
2026-03-05 15:12:28 -0300

    atualizando site

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function mostrarMensagem(){
     document.getElementById("mensagem").innerText = texto
     
     }
+    
+    
+    // CONTADOR DE DIAS
+    function atualizarContador(){
+    
+    const dataInicio = new Date("2025-12-12");
+    const hoje = new Date();
+    
+    const diferenca = hoje - dataInicio;
+    
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    
+    document.getElementById("contador").innerHTML =
+    "Estamos juntos há <br><b>" + dias + " dias ❤️</b>";
+    
+    }
+    
+    atualizarContador();
